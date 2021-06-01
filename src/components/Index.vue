cf89bebcb23aec1631735b71c3815fd8e60085c0
Andrey Cherepanov <andreykauf@gmail.com>
2021-06-01 10:55:56 +0700

    add spinner

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div v-if="init">
-    Loading...
-  </div>
+  <ChatNotificationsMessage v-if="init" heading="Подключение к сети…" subheading="Загрузка сообщений и проверка соединения" spinner />
   <Chat v-else :channel-id="channelId" />
 </template>
 
@@ -10,10 +8,12 @@
 import { mapState } from 'vuex';
 
 import Chat from './Chat.vue';
+import ChatNotificationsMessage from './ChatNotifications/ChatNotificationsMessage.vue';
 
 export default {
   components: {
     Chat,
+    ChatNotificationsMessage,
   },
   props: {
     channelId: { type: [String, Number], required: true },
